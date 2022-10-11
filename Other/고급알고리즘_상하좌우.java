b259f9097d65748ebbd2ad4c2fe9e0e41a3ba2f0
dev_whale <jeong22437747@gmail.com>
2022-10-11 12:12:32 +0900

    고급알고리즘 문제 수정

--- Other/고급알고리즘_상하좌우.java
+++ Other/고급알고리즘_상하좌우.java
@@ -1,26 +1,35 @@
 
+import javax.swing.text.Style;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-public class main {
+public class 고급알고리즘_상하좌우 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] dir = {{0, -1}, {0,1}, {-1, 0}, {-1,0} };
         char[] alpha = {'L', 'R', 'U', 'D'};
         int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
-        int x=0; y = 0;
+        int x=0;
+        int y = 0;
         for(int i=0; i < input.length; i++){
             for(int j =0; j < 4; j++){
-                if(input[i] == alpha[j]){
+                if(input[i].charAt(0) == alpha[j]){
                     int nx  = x + dir[0][j];
                     int ny = y + dir[1][j];
+                    if(nx < 1 || ny < 1 || nx > n || ny > n){
+                        continue;
+                    }
+                    x = nx;
+                    y = ny;
                 }
             }
             
         }
-        if(nx < 1 || ny < 1 || nx > n || ny > n){
-            continue;
-        }
-        System.out.Println(x + ", " + y);
+
+        System.out.println(x + ", " + y);
     }
 }
 
