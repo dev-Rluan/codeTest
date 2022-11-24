32e2b680ab564f93c134ccff69ee700ec0070e53
dev_whale <jeong22437747@gmail.com>
2022-11-24 19:31:20 +0900

    20221123-2

--- Other/고급알고리즘_비트_대소문자변형.java
+++ Other/고급알고리즘_비트_대소문자변형.java
@@ -1,14 +1,14 @@
 class 고급알고리즘_비트_대소문자변형{
     public static void main(String[] args){
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.redLine();
-        System.out.println(toggleCase(str));
-    }
-
-    public static String toggleCase(String str){
-        for(int i =0; i< str.length(); i++){
-            str.charAt(i) ^=32;
-        }
-        return str;
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.redLine();
+//        System.out.println(toggleCase(str));
+//    }
+//
+//    public static String toggleCase(String str){
+//        for(int i =0; i< str.length(); i++){
+//            str.charAt(i) ^=32;
+//        }
+//        return str;
     }
 }
